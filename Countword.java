2a5cd9f3de376c9ce53b801f5dafb8176a3d696a
Nandha kumar S <mailstonandhakumar@gmail.com>
2018-09-03 13:50:47 +0530

    Update and rename README.md to Countword.java

---
+++ Countword.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+  public class CountWord{
+      public static void main(String args[]){
+        Scanner sc=new scanner(system.in);
+        System.out.println("Enter your String");
+        String input=sc.nextLine();
+        int count=`1;
+          for(int i=0;i<input.length()-1;i++)
+          {
+            if(input.charAt(i)==' '&& input.charAt(i+1))
+            {
+              count++;
+            }
+          }  
+       }
+     }  
+        
